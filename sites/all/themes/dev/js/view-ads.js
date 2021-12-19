1face72f608893cd0ea9c176f7e623150f78c503
Production <admin@horki.info>
2021-12-19 16:39:49 +0300

    Removed dev theme

--- sites/all/themes/dev/js/view-ads.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    if($('.form-type-select select').length){
-        $('.form-type-select select').change(function() {
-            $(this).parents('form').submit();
-        });
-    }
-});
